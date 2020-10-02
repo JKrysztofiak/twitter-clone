4b607b77436659fcf1bda95cd9b0a25953005521
JKrysztofiak <jakub.krysztofiak@protonmail.com>
2020-10-02 19:12:50 +0200

    feed updating after adding a message working!

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,24 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {MessagesService} from '../messages.service';
-import {MessageDTO} from '../messageDTO';
-import {Observable} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { MessagesService } from '../messages.service';
+import { MessageDTO } from '../messageDTO';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: 'app-feed',
-  templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+	selector: 'app-feed',
+	templateUrl: './feed.component.html',
+	styleUrls: [ './feed.component.css' ]
 })
 export class FeedComponent implements OnInit {
-  constructor(private messagesService: MessagesService) {
-  }
+	constructor(private messagesService: MessagesService) {}
 
-  messages: Observable<MessageDTO[]>;
+	messages = new BehaviorSubject<MessageDTO[]>([]);
 
-  ngOnInit(): void {
-    this.messages = this.getMessages();
-  }
-
-  getMessages(): Observable<MessageDTO[]> {
-    return this.messagesService.getMessages();
-  }
+	ngOnInit(): void {
+		this.messagesService.subscribe(this.messages);
+	}
 }
--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -1,49 +1,55 @@
-import {Injectable} from '@angular/core';
-import {Message} from './message-template';
-import {MessageDTO} from './messageDTO';
+import { Injectable } from '@angular/core';
+import { Message } from './message-template';
+import { MessageDTO } from './messageDTO';
 
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable, of, Observer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class MessagesService {
-  messages: Observable<MessageDTO[]>;
+	messages: MessageDTO[];
+	messagesSubject = new BehaviorSubject<MessageDTO[]>([]);
 
-  baseUrl = 'https://localhost:44378/api/messages';
-  httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
-  };
+	baseUrl = 'https://localhost:44378/api/messages';
+	httpOptions = {
+		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	};
 
-  constructor(private http: HttpClient) {
-    this.messages = of([]);
-  }
+	constructor(private http: HttpClient) {
+		this.getMessages().subscribe((arr) => {
+			this.messages = arr;
+			this.update();
+		});
+	}
 
-  // Serwis odbiera wiadomość od "feed"
-  // TODO: Update feed after adding a message
-  addMessage(message: Message): Observable<Message> {
-    if (!message.messageText) {
-      return;
-    }
-    if (!message.username) {
-      message.username = 'Anonymous'; // jeżeli username nie został podany
-    }
-    this.messages.pipe(map(source => {
-      source.push({
-        messageId: 0,
-        username: message.username,
-        messagetext: message.messageText
-      });
-      console.log('akcja!', source);
-    })).subscribe();
-    // this.messages.push();
-    return this.http.put<Message>(this.baseUrl, message, this.httpOptions);
-  }
+	subscribe(observer: Observer<MessageDTO[]>) {
+		this.messagesSubject.subscribe(observer);
+	}
 
-  getMessages(): Observable<MessageDTO[]> {
-    // return this.http.get<MessageDTO[]>(this.baseUrl);
-    return this.messages;
-  }
+	// Serwis odbiera wiadomość od "feed"
+	// TODO: Update feed after adding a message
+	addMessage(message: Message): Observable<Message> {
+		if (!message.messageText) {
+			return;
+		}
+		if (!message.username) {
+			message.username = 'Anonymous'; // jeżeli username nie został podany
+		}
+
+		this.http.put<MessageDTO>(this.baseUrl, message, this.httpOptions).subscribe((msg) => {
+			this.messages.unshift(msg);
+			this.update();
+		});
+	}
+
+	getMessages(): Observable<MessageDTO[]> {
+		return this.http.get<MessageDTO[]>(this.baseUrl);
+	}
+
+	update() {
+		this.messagesSubject.next(this.messages);
+	}
 }
